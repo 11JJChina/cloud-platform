16d5a0636c807a17b186b37d26d8d8e9a9332694
zhangguowne <zhangguowen@tallsafe.com>
2019-07-30 18:55:09 +0800

    微服务模板项目

--- ace-modules/ace-admin/src/main/java/com/github/wxiaoqi/security/admin/config/WebConfiguration.java
+++ ace-modules/ace-admin/src/main/java/com/github/wxiaoqi/security/admin/config/WebConfiguration.java
@@ -6,6 +6,9 @@ import com.github.wxiaoqi.security.common.handler.GlobalExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -61,6 +64,25 @@ public class WebConfiguration implements WebMvcConfigurer {
         };
         Collections.addAll(list, urls);
         return list;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration conf = new CorsConfiguration();
+        conf.addAllowedHeader("*");
+        conf.addAllowedMethod("*");
+        conf.addAllowedOrigin("*");
+        conf.setAllowCredentials(true);
+        conf.setMaxAge(3600L);
+        conf.addExposedHeader("set-cookie");
+        conf.addExposedHeader("access-control-allow-headers");
+        conf.addExposedHeader("access-control-allow-methods");
+        conf.addExposedHeader("access-control-allow-origin");
+        conf.addExposedHeader("access-control-max-age");
+        conf.addExposedHeader("X-Frame-Options");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", conf); // 4 对接口配置跨域设置
+        return new CorsFilter(source);
     }
 
 }
